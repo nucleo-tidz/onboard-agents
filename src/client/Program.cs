23cba64f55f0807c17693fd9da9dceaf935e40ed
ahmar husain <nucleotidz@hotmail.com>
2025-05-26 00:38:58 +0530

    fix

--- src/client/Program.cs
+++ src/client/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Hosting;
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((config, services) =>
     {
-        services.AddSemanticKernel(config.Configuration, false);
+        services.AddSemanticKernel(config.Configuration);
 #pragma warning disable SKEXP0110 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
         services.AddTransient<IBootStrapper, BootStrapper>();
 #pragma warning restore SKEXP0110 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
